6d046078b7662c96fa5c5669ffa3f3f569159b08
Sam Dornan <sdornan@gmail.com>
2014-10-17 13:53:51 -0500

    Tweak to 2fa controller

--- app/controllers/devise/two_factor_authentication_controller.rb
+++ app/controllers/devise/two_factor_authentication_controller.rb
@@ -26,12 +26,13 @@ class Devise::TwoFactorAuthenticationController < DeviseController
         resource.errors.add(:base, 'Access completely denied as you have reached your attempts limit.')
         respond_to do |format|
           format.html { render :max_login_attempts_reached }
-          format.json { render json: :nothing, status: :forbidden }
+          format.json { render json: resource, status: :forbidden }
         end
       else
+        resource.errors.add(:base, 'The code entered is incorrect.')
         respond_to do |format|
           format.html { render :show }
-          format.json { render json: :nothing, status: :forbidden }
+          format.json { render json: resource, status: :forbidden }
         end
       end
     end
@@ -51,7 +52,7 @@ class Devise::TwoFactorAuthenticationController < DeviseController
         resource.errors.add(:base, 'Access completely denied as you have reached your attempts limit.')
         respond_to do |format|
           format.html { render :max_login_attempts_reached and return }
-          format.json { render json: :nothing, status: :forbidden and return }
+          format.json { render json: resource, status: :forbidden and return }
         end
       end
     end
